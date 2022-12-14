13f88fb06280a4c9d56cf949ce59b82763129347
oljas <olja.smirnov93@gmail.com>
2022-12-14 21:56:49 +0200

    adds HW#9 button and hidden

--- src/cities.js
+++ src/cities.js
@@ -1,11 +1,21 @@
 const detailsImage = document.querySelector(".details-image");
 const detailsTitle = document.querySelector(".details-title");
 const anchorElements = document.querySelectorAll(".thumbnails-anchor");
+const mainElement = document.querySelector(".main-class")
+const HIDDEN = "hidden";
+function showDetails(){
+    mainElement.classList.remove(HIDDEN);
+}
+function hideDetails(){
+    mainElement.classList.add(HIDDEN);
+}
 function setDetails(anchor) {
     const dataImage = anchor.getAttribute("data-details-image");
     detailsImage.src = dataImage;
+    showDetails();
     const dataTitle = anchor.getAttribute("data-details-title");
     detailsTitle.innerHTML = dataTitle;
+    
 }
 for (let i = 0; i < anchorElements.length; i++) {
     anchorElements[i].addEventListener("click", function(){
