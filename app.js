b6edcc12cb32f80b351c3c80fe0c01ba36511be7
oljas <olja.smirnov93@gmail.com>
2023-01-21 19:02:32 +0200

    HW #19 anagram and counter of countries

--- app.js
+++ app.js
@@ -12,72 +12,48 @@ const employees = [
     createEmployee(129, "Sasha", 2000, 25000, "Ramat Gan", "Israel"),
     createEmployee(130, "Victor", 2003, 10000, "Arad", "Israel")
 ]
-//const index = employees.indexOf(createEmployee(126, "Abraham", 1990, 13000, "London", "UK")); //not right
-const index = employees.findIndex(function(empl){
-    return empl.id === 126;
-})
-const employee = employees.find(function(empl){
-    return empl.id === 126;
-})
-//HW #18
-function getEmployee(employees, id){
-    const employee = employees.find(function(empl){
-        return empl.id === id;
-    })
-    return employee;
+//HW #19
+//returns country with most amount of employees
+function getMostPopulatedCountry(employees){
+    let res = "";
+    const objContries = getContriesOccurrences(employees);
+    const arrayCountryAndNumber = Object.entries(objContries);
+    arrayCountryAndNumber.sort((country1, country2) => country2[1] - country1[1] );
+    res = arrayCountryAndNumber[0][0];
+    return res;
 }
-let id = 126
-console.log(`Employee has id: ${id}`)
-console.log(getEmployee(employees, id));
-
-function getEmployeesBySalary(employees, salaryFrom, salaryTo){
-    const arrayEmployees = employees.filter(function(empl){
-        return empl.salary >= salaryFrom && empl.salary <= salaryTo;
-    })
-    return arrayEmployees;
-    
+function getContriesOccurrences(employees){
+    const res = {};
+    employees.forEach(empl => {
+        !res[empl.adress.country] ? res[empl.adress.country] = 1 : res[empl.adress.country]++;
+    });
+    return res;
 }
-let salaryFrom = 13000;
-let salaryTo = 15000
-console.log(`Employees with salary from ${salaryFrom} to ${salaryTo}:`);
-console.log(getEmployeesBySalary(employees, salaryFrom, salaryTo))
+console.log(`Country with most amount of employees is "${getMostPopulatedCountry(employees)}"`);
 
-function getEmployeesByCity(employees, city){
-    const arrayEmployees = employees.filter(function(empl){
-        return empl.adress.city === city;
-    })
-    return arrayEmployees;
+//returns a given number (counter) of countries with most amount of employees
+function getMostPopulatedCountries(employees, counter) {
+    const res = [];
+    const objContries = getContriesOccurrences(employees);
+    const arrayCountryAndNumber = Object.entries(objContries);
+    arrayCountryAndNumber.sort((country1, country2) => country2[1] - country1[1] );
+    for(let i = 0; i < counter; i++){
+        res[i] = arrayCountryAndNumber[i][0];
+    }
+    return res;
 }
-let city = "New York";
-
-console.log(`Employees from city: ${city} are`);
-console.log(getEmployeesByCity(employees, city));
-
-function getEmployeeNames(employees){
-    const namesOfEmployees = employees.map(function(empl){
-        return empl.name;
-    })
-    return namesOfEmployees
-
-    //return employees.map(empl => empl.name); looked at another work
+let counter = 3;
+console.log(`${counter} country/ies with most amount of employees is "${getMostPopulatedCountries(employees, counter).join(", ")}"`);
+//returns true if a given anagram is indeed an angram of a given word
+function isAnagram(word, anagram){
+    let res = false;
+    if (word.length === anagram.length){
+        const str1 = word.split("").sort().join(""); // split() make an array from string
+        const str2 = anagram.split("").sort().join(""); //join() make a string from array
+        res = (str1 === str2); 
+    }
+    return res ? "Yes, it's true" : "No, it's false";
 }
-console.log(`Names of all employees: ${getEmployeeNames(employees).join(" , ")}`);
-
-function sortEmployeesByAge(employees){
-    const arrayByAge = employees.sort(function(empl1, empl2){
-        return empl2.birthYear - empl1.birthYear;
-    })
-    return arrayByAge;
-}
-console.log(`Employee objects sorted by age in ascending order`);
-console.log(sortEmployeesByAge(employees));
-
-function computeSalaryBudget(employees){
-    const totalSalary = employees.reduce(function(res, empl){
-        return res + empl.salary;
-    }, 0)
-    return totalSalary;
-    
-    //return employees.reduce((res, empl) => res + empl.salary, 0); looked at another work
-}
-console.log(`Total salary = ${computeSalaryBudget(employees)}`);
+let word = "marry";
+let anagram = "ramry";
+console.log(`'${anagram}' is angram of a word '${word}'? "${isAnagram(word, anagram)}"`);
